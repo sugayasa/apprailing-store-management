70c4adcf538f927e91bdfae74e1de6e13e77fc33
Agus Adiyasa <agus.adiyasa@gmail.com>
2026-02-21 06:39:55 +0700

    Update Initial Commit

--- app/Views/ContentMain/systemSetting.php
+++
@@ -1,2 +0,0 @@
-<div class="d-lg-flex content-menu" id="content-systemSetting">
-</div>
--- app/Views/ContentPills/systemSetting.php
+++
@@ -1,4 +0,0 @@
-<div class="tab-pane fade show active" id="pills-settings" role="tabpanel" aria-labelledby="pills-settings-tab">
-    <div>
-    </div>
-</div>
